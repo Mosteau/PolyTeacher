befcff9413a1fb3f792612057aa4132780a024b3
Thibaut Mosteau <thibaut.mosteau@lilo.org>
2024-11-15 14:11:56 +0100

    ia int

---
+++ sandbox/gemini.py
@@ -0,0 +1,12 @@
+import google.generativeai as genai
+
+
+# configuration de gimini IA pour l'application DJANGO Python
+# configuration de la clef API
+api_key=""
+genai.configure(api_key=api_key)
+
+# génération de texte
+model = genai.GenerativeModel("gemini-1.5-flash")
+response = model.generate_content("Comment on dit oiseau en anglais ?")
+print(response.text)
